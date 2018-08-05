063c5d6669eb0c0969478a9a307ac4959e1be59b
David Fernández Aldana <luskaner@gmail.com>
2018-08-05 18:52:38 +0200

    Increased test function timeout from 3m. to 5m

--- tests/TestStaticListRecords.py
+++ tests/TestStaticListRecords.py
@@ -23,56 +23,56 @@ class TestStaticListRecords(TestListRecords):
             'TEST_FAIL_END': Sickle(self.get_server_url() + "/TEST_FAIL_END")
         }
 
-    @timeout_decorator.timeout(180)
+    @timeout_decorator.timeout(300)
     def test_from_until_no_set_start(self):
         self._test(
             "TEST_NO_SET_START",
             {'metadataPrefix': 'oai_dc', 'from': '2016-01-01', 'until': '2016-02-01'}
         )
 
-    @timeout_decorator.timeout(180)
+    @timeout_decorator.timeout(300)
     def test_until_no_set_start(self):
         self._test(
             "TEST_NO_SET_START",
             {'metadataPrefix': 'oai_dc', 'until': '2009-10-01'}
         )
 
-    @timeout_decorator.timeout(180)
+    @timeout_decorator.timeout(300)
     def test_from_no_set_start(self):
         self._test(
             "TEST_NO_SET_START",
             {'metadataPrefix': 'oai_dc', 'from': '2018-07-02'}
         )
 
-    @timeout_decorator.timeout(180)
+    @timeout_decorator.timeout(300)
     def test_from_until_no_set_end(self):
         self._test(
             "TEST_NO_SET_END",
             {'metadataPrefix': 'oai_dc', 'from': '2016-01-01', 'until': '2016-02-01'}
         )
 
-    @timeout_decorator.timeout(180)
+    @timeout_decorator.timeout(300)
     def test_until_no_set_end(self):
         self._test(
             "TEST_NO_SET_END",
             {'metadataPrefix': 'oai_dc', 'until': '2009-10-01'}
         )
 
-    @timeout_decorator.timeout(180)
+    @timeout_decorator.timeout(300)
     def test_from_no_set_end(self):
         self._test(
             "TEST_NO_SET_END",
             {'metadataPrefix': 'oai_dc', 'from': '2018-07-02'}
         )
 
-    @timeout_decorator.timeout(180)
+    @timeout_decorator.timeout(300)
     def test_fail_start(self):
         self._test(
             "TEST_FAIL_START",
             {'metadataPrefix': 'oai_dc', 'from': '2018-07-02'}
         )
 
-    @timeout_decorator.timeout(180)
+    @timeout_decorator.timeout(300)
     def test_fail_end(self):
         self._test(
             "TEST_FAIL_END",
